ff14faf4c3bb0514368cce695d35ba24e6a7caa5
N1vas-B <155220531+N1vas-B@users.noreply.github.com>
2025-03-09 18:51:06 +0530

    Delete Bookstore directory

--- Bookstore/src/main/java/com/book/store/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.book.store.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.book.store.dao.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
-
-}
